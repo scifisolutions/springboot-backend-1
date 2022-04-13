15cb0858655f03c6c854a08c0ba6146608bf98b4
ajayakiwate <ajayakiwatesrp@gmail.com>
2022-04-13 17:02:20 +0530

    First Commit

---
+++ src/main/java/springboot/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springboot.model.Employee;
+import springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	//get all employees
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		
+		return employeeRepository.findAll();
+	}
+}
